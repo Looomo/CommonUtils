b65e854a306afc1fe23ef64416505b3d7abac16a
单怡翔 <bigsyx@126.com>
2023-10-19 13:12:15 +0800

    fix install error

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
   name = 'CommonUtils',
   packages = find_packages(),
-  version = "0.0.1"
+  version = "0.0.1",
+  py_modules = ['CommonUtils'],
 )
