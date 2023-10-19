b9926e693449928c63e265d559f3988a3e37c9ff
单怡翔 <bigsyx@126.com>
2023-10-19 13:26:04 +0800

    add gitignore, default is output to no logfile.

--- CommonUtils.py
+++ CommonUtils.py
@@ -8,6 +8,7 @@ def ch_cwd():
     current_file_name = inspect.getfile(inspect.currentframe())
     path = os.path.dirname(os.path.abspath(current_file_name))
     os.chdir(path)
+    print(f"Changing working directory to {path}.")
 
 def set_seed(seed):
     try:
@@ -24,22 +25,28 @@ def set_seed(seed):
     except:
         print("Fails to set seeds.")
 
-def getlogger(logfile = "log.log", log_format = "[%(asctime)s] [%(levelname)s] - %(message)s", logger_name = "my_logger"):
+def getlogger(logfile = None, log_format = "[%(asctime)s] [%(levelname)s] - %(message)s", logger_name = "my_logger"):
     
     logger = logging.getLogger(logger_name)
 
     formatter = logging.Formatter(log_format)
 
-    file_handler = logging.FileHandler(logfile)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
+
+
+    if logfile is not None:
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.warning(f"Outputing log to {logfile}.")
+    else:
+        logger.warning("Not outputing to logfile.")
+    
+    
     logger.setLevel(logging.DEBUG)
 
     return logger
